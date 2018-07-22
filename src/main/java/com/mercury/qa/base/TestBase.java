f473baced31320de3ecaf4bede22822fb2640af9
U47317 <arunmohanmammen@yahoo.com>
2018-07-22 13:28:01 +0530

    minor changes

--- src/main/java/com/mercury/qa/base/TestBase.java
+++ src/main/java/com/mercury/qa/base/TestBase.java
@@ -3,7 +3,6 @@ package com.mercury.qa.base;
 import java.io.FileInputStream;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
